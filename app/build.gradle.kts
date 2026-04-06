cba0567d9544f024da272255c68e5b84d3a14e07
gncai_admin <gncai.code@gmail.com>
2026-04-06 13:44:14 +0900

    버전코드 5로 올림

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.cnanfc.xcanplayer_final2"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
+        versionCode = 5
         versionName = "1.0"
     }
 
